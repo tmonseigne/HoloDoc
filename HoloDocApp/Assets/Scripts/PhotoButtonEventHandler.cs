80696f49676010a37d740175cc1a36090f94735c
PALARD Nicolas <nicolas.palard@etu.u-bordeaux.fr>
2018-01-28 00:05:22 +0100

    :sparkles: Added a new button using HoloToolkit.

---
+++ HoloDocApp/Assets/Scripts/PhotoButtonEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using HoloToolkit.Unity.InputModule;
+
+public class PhotoButtonEventHandler : MonoBehaviour, IFocusable, IInputClickHandler {
+
+    private AudioSource m_audio;
+
+    void Start()
+    {
+        m_audio = this.GetComponent<AudioSource>();
+    }
+
+    public void OnFocusEnter()
+    { }
+
+    public void OnFocusExit()
+    {
+        m_audio.Stop();
+    }
+
+    public void OnInputClicked(InputClickedEventData data)
+    {
+        m_audio.clip = Resources.Load<AudioClip>("Sounds/Button_Press");
+        m_audio.Play();
+        this.GetComponent<PhotoScript>().TakePhoto();
+    }
+}
---
+++ HoloDocApp/Assets/Scripts/PhotoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoScript : MonoBehaviour {
+
+    public AudioClip shutterSound;
+    // Use this for initialization
+    public void TakePhoto()
+    {
+        this.GetComponent<AudioSource>().clip = shutterSound;
+        Debug.Log("Photo");
+        StartCoroutine(Wait());
+        
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(1);
+        this.GetComponent<AudioSource>().Play();
+    }
+}
