bd79f018d77bb82a8574d053ba2ba297205b669a
Arnaud Delpeyroux <delpeyroux.arnaud@gmail.com>
2018-02-01 10:37:17 +0100

    :construction: WIP: Added a first version of the app menu
    Will change ;)

---
+++ HoloDocApp/Assets/Scripts/EnterButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.SceneManagement;
+
+public class EnterButton : MonoBehaviour, IFocusable, IInputClickHandler
+{
+
+    void Start()
+    {
+    }
+
+    public void OnFocusEnter()
+    { }
+
+    public void OnFocusExit()
+    {
+    }
+
+    public void OnInputClicked(InputClickedEventData data)
+    {
+        Debug.Log("Enter");
+        SceneManager.LoadScene("HoloDoc");
+    }
+}
---
+++ HoloDocApp/Assets/Scripts/IpButton.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using HoloToolkit.UI.Keyboard;
+using HoloToolkit.Unity.InputModule;
+
+public class IpButton : MonoBehaviour, IFocusable, IInputClickHandler
+{
+
+    
+    public static string ipAdress = "";
+
+    void Awake()
+    {
+        Keyboard.Instance.OnTextUpdated += Keyboard_OnTextUpdated;
+        Keyboard.Instance.OnClosed += Keyboard_OnClosed;
+    }
+
+    /// <summary>
+    /// Delegate function for getting keyboard input
+    /// </summary>
+    /// <param name="newText"></param>
+    private void Keyboard_OnTextUpdated(string newText)
+    {
+        if (!string.IsNullOrEmpty(newText))
+        {
+            ipAdress = newText;
+        }
+    }
+
+    /// <summary>
+    /// Delegate function for getting keyboard input
+    /// </summary>
+    /// <param name="sender"></param>
+    private void Keyboard_OnClosed(object sender, EventArgs e)
+    {
+        // Unsubscribe from delegate functions
+        Keyboard.Instance.OnTextUpdated -= Keyboard_OnTextUpdated;
+        Keyboard.Instance.OnClosed -= Keyboard_OnClosed;
+    }
+
+
+    private void Keyboard_OnTextSubmitted(object sender, System.EventArgs e)
+    {
+        Keyboard.Instance.Close();
+    }
+
+    public void OnFocusEnter()
+    { }
+
+    public void OnFocusExit()
+    {
+    }
+    
+
+    public void OnInputClicked(InputClickedEventData data)
+    {
+        // Single-line textbox
+        Keyboard.Instance.Close();
+        Keyboard.Instance.PresentKeyboard(ipAdress);
+    }
+}
