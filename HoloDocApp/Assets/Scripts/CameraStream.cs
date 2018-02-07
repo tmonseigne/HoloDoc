549a2f3cd8d280b8b32a024009686d1ebba15518
PALARD Nicolas <nicolas.palard@etu.u-bordeaux.fr>
2018-02-07 19:22:13 +0100

    :construction: Started document prefab & outline shader

--- HoloDocApp/Assets/Scripts/CameraStream.cs
+++ HoloDocApp/Assets/Scripts/CameraStream.cs
@@ -10,6 +10,7 @@ public class CameraStream : MonoBehaviour
 
     [Tooltip("You can provide a substitution frame if you do not have a camera or for testing purpose.")]
     public Texture2D substituableFrame;
+
     [Tooltip("If you have a camera and you still want to use a special substitution frame you can force the substitution.")]
     public bool substitute = false;
 
@@ -61,7 +62,8 @@ public class CameraStream : MonoBehaviour
             cameraFrame = new WebCamTexture(frameResolution.width, frameResolution.height);
             cameraFrame.Play();
             frame = new Frame(frameResolution, cameraFrame.GetPixels32());
-        } else
+        }
+        else
         {
             throw new System.Exception("No camera/substitution frame found.");
         }
---
+++ HoloDocApp/Assets/Scripts/DocumentMesh.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter))]
+public class DocumentMesh : MonoBehaviour {
+
+    public Material material;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateDocumentMesh(Vector3[] corners)
+    {
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = corners;
+
+        int[] faces = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2,
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = faces;
+
+        this.GetComponent<MeshRenderer>().material = material;
+        this.GetComponent<MeshFilter>().mesh = mesh;
+    }
+}
