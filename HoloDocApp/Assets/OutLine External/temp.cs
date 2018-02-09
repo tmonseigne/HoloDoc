80047e0c506c8b81613d1103a27b07c4b12c75c7
Arnaud Delpeyroux <delpeyroux.arnaud@gmail.com>
2018-02-09 13:28:38 +0100

    :construction: Improving document prefab by adding outline material

---
+++ HoloDocApp/Assets/OutLine External/temp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp : MonoBehaviour {
+	public float width;
+	public float height;
+
+	void Start() {
+		MeshFilter mf = GetComponent<MeshFilter>();
+		mf.mesh = CreateMesh (width, height);
+	}
+
+
+	Mesh CreateMesh(float w, float h)
+	{
+		Mesh m = new Mesh();
+		m.name = "ScriptedMesh";
+		m.vertices = new Vector3[] {
+			new Vector3(-w, -h, 0),
+			new Vector3(w, -h, 0),
+			new Vector3(w, h, 0),
+			new Vector3(-w, h, 0)
+		};
+		m.uv = new Vector2[] {
+			new Vector2 (0, 0),
+			new Vector2 (0, 1),
+			new Vector2(1, 1),
+			new Vector2 (1, 0)
+		};
+		m.triangles = new int[] { 0, 1, 2, 0, 2, 3};
+		m.RecalculateNormals();
+
+		return m;
+	}
+}
--- HoloDocApp/Assets/TestInstanciate.cs
+++ HoloDocApp/Assets/TestInstanciate.cs
@@ -15,8 +15,21 @@ public class TestInstanciate : MonoBehaviour {
             new Vector3(1, 0, 0),
             new Vector3(1, 1, 0),
         };
+
+        Vector3 centroid3f = Vector3.zero;
+
+        foreach (Vector3 v in corners)
+        {
+            centroid3f += v;
+        }
+        centroid3f /= corners.Length;
+        Vector4 centroid4f = new Vector4(centroid3f.x, centroid3f.y, centroid3f.z, 1.0f);
+        Debug.Log(centroid4f);
+
         go.GetComponent<DocumentMesh>().CreateDocumentMesh(corners);
-	}
+        go.GetComponent<DocumentMesh>().material.SetVector("_centroid", centroid4f);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
