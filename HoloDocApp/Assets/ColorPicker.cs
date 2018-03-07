88a08c27ef10062fe3523644a6e512fa1835a509
Nicolas PALARD <palardnicolas@gmail.com>
2018-03-07 09:58:13 +0100

    :construction: Average color computation and display (debug)

---
+++ HoloDocApp/Assets/ColorPicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+using HoloToolkit.Unity;
+
+public class ColorPicker : Singleton<ColorPicker> {
+
+    [Range(0.1f, 1.0f), 
+     Tooltip("This property will force the color picking to be done in a specific area of the image. " +
+        "By modifying this property you can control how much percentage of the image will be considered " +
+        "when returning average color.")]
+    public float PickingAreaPercentage = 0.5f;
+
+    [Tooltip("This boolean is used to force the area to be squared.")]
+    public bool forceSquared = false;
+
+    public Color AverageColor(Texture2D texture)
+    {
+        int blockWidth = (int)(texture.width * PickingAreaPercentage);
+        int blockHeight = (int)(texture.height * PickingAreaPercentage);
+
+        if (forceSquared) {
+            if (blockWidth > blockHeight) {
+                blockWidth = blockHeight;
+            }
+            else {
+                blockHeight = blockWidth;
+            }
+        }
+
+        int startX = texture.width / 2 - blockWidth / 2;
+        int startY = texture.height / 2 - blockHeight / 2;
+
+        Color[] bloc = texture.GetPixels(startX, startY, blockWidth, blockHeight);
+        Color average = new Color();
+        foreach (Color c in bloc) {
+            average += c;
+        }
+
+        average /= bloc.Length;
+        return average;
+    }
+}
--- HoloDocApp/Assets/EventManager.cs
+++ HoloDocApp/Assets/EventManager.cs
@@ -2,15 +2,92 @@
 using HoloToolkit.Unity.InputModule;
 
 public class EventManager : MonoBehaviour, IInputClickHandler
-{
-    public void OnPhotoTaken(Texture photo, Matrix4x4 proj, Matrix4x4 world, bool projB, bool worldB)
+{ 
+    public GameObject outputBloc;
+    public GameObject outputColor;
+
+    public void PickColor(Texture2D photo)
+    {
+        float percentage = 0.5f;
+        int blockWidth = (int) (photo.width * percentage);
+        int blockHeight = (int) (photo.height * percentage);
+
+        if (blockWidth > photo.height || blockWidth > blockHeight)
+        {
+            blockWidth = blockHeight;
+        }
+
+        if (blockHeight > photo.width || blockHeight > blockWidth) ;
+        {
+            blockHeight = blockWidth;
+        }
+
+        Debug.Log(blockHeight);
+        Debug.Log(blockWidth);
+        int startX = photo.width / 2 - blockWidth / 2;
+        int startY = photo.height / 2 - blockHeight / 2;
+        Color[] background = photo.GetPixels(startX, startY, blockWidth, blockHeight);
+
+        Texture2D extracted = new Texture2D(blockWidth, blockHeight);
+        extracted.SetPixels(background);
+        extracted.Apply(true);
+
+        outputBloc.GetComponent<Renderer>().material.mainTexture = extracted;
+
+        float r = 0;
+        float g = 0;
+        float b = 0;
+        foreach ( Color pixel in background)
+        {
+            r += pixel.r;
+            g += pixel.g;
+            b += pixel.b;
+        }
+
+        r /= background.Length;
+        g /= background.Length;
+        b /= background.Length;
+
+        Color finalColor = new Color(r, g, b);
+
+        Debug.LogFormat("Average background color is r:{0} g:{1} b:{2}", r, g, b);
+
+        Texture2D test = new Texture2D(100, 100);
+        Color[] pixels = test.GetPixels();
+        for(int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = finalColor;
+        }
+
+        test.SetPixels(pixels);
+        test.Apply(true);
+
+        outputColor.GetComponent<Renderer>().material.mainTexture = test;
+    }
+
+
+    public void OnPhotoTaken(Texture2D photo, Matrix4x4 proj, Matrix4x4 world, bool projB, bool worldB)
     {
         Debug.Log(projB);
         Debug.Log(worldB);
         Debug.Log(proj.ToString());
         Debug.Log(world.ToString());
 
-        Debug.Log("Callback");
+        //PickColor(photo);
+        Color c = ColorPicker.Instance.AverageColor(photo);
+        Debug.Log(c);
+
+        Texture2D test = new Texture2D(1, 1);
+        Color[] pixels = test.GetPixels();
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = c;
+        }
+
+        test.SetPixels(pixels);
+        test.Apply(true);
+
+        outputColor.GetComponent<Renderer>().material.mainTexture = test;
     }
 
     public void OnInputClicked(InputClickedEventData eventData)
