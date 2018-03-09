d9d323da5481fe3925991f6b6bf9b731d1094f14
Arnaud Delpeyroux <arnaud.delpeyroux@live.com>
2018-03-09 19:06:21 +0100

    :rotating_light: Adding unit testing framework on the server

---
+++ HoloDocServer/test/test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+
+describe('Testing unit testing - ', function() {
+  
+  
+  beforeEach(function(done) {
+    done();
+  });
+
+  afterEach(function(done) {
+    done();
+  });
+
+  describe('Verifying that it is working:', function () {
+    it('Assert false', function (done) {
+      assert(false);
+      done();
+    });
+    it('Assert True', function (done) {
+      assert(true);
+      done();
+    });
+  });
+  
+});
