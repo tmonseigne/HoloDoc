4f30d88faf732f418b7bd3170cfc5d061331a4af
Arnaud Delpeyroux <delpeyroux.arnaud@gmail.com>
2018-03-14 20:01:17 +0100

    :sparkles: Added detection/undistorsion on the server

---
+++ HoloDocServer/tests/doc-detection-test.js
@@ -0,0 +1,26 @@
+const cv = require('opencv4nodejs');
+const improc = require('../improc/improc.js');
+
+let image = cv.imread('./D.jpg');
+
+let bidule = improc.detectDocuments(image, [25, 25, 25]);
+
+let colors = [new cv.Vec(255, 0, 0),
+              new cv.Vec(0, 255, 0),
+              new cv.Vec(0, 0, 255),
+              new cv.Vec(255, 255, 0)]
+
+bidule.forEach (function (points) {
+  points.forEach (function (point, index) {
+    image.drawCircle(point, 4, colors[index], -1);
+  });
+});
+
+cv.imwrite('result.jpg', image);
+
+let center = improc.getCenter(image);
+let toExtract = improc.getNearestdocFrom(bidule, center);
+
+let croped = improc.undistordDoc(image, toExtract);
+
+cv.imwrite('final.jpg', croped);
